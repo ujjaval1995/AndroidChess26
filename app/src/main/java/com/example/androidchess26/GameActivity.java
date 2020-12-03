5694da1868863ff08f48445dfafa1021e54c89e3
Ujjaval Shah <us89@scarletmail.rutgers.edu>
2020-12-03 14:07:14 -0500

    minor changes to GameActivity(not working)

--- app/src/main/java/com/example/androidchess26/GameActivity.java
+++ app/src/main/java/com/example/androidchess26/GameActivity.java
@@ -2,14 +2,67 @@ package com.example.androidchess26;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class GameActivity extends AppCompatActivity
 {
+    Button btnAI;
+    Button btnRollback;
+    Button btnDraw;
+    Button btnResign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        btnAI = findViewById(R.id.btnAI);
+        btnRollback = findViewById(R.id.btnRollback);
+        btnDraw = findViewById(R.id.btnDraw);
+        btnResign = findViewById(R.id.btnResign);
+
+        btnAI.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+               // Intent intent = new Intent(MainActivity.this, GameActivity.class);
+               // startActivity(intent);
+            }
+        });
+
+        btnRollback.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                // startActivity(intent);
+            }
+        });
+
+        btnDraw.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                // Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                // startActivity(intent);
+            }
+        });
+
+        btnResign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                // startActivity(intent);
+            }
+        });
     }
 }
