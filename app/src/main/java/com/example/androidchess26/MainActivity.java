90e00bae829accd1c9672412f8139f50c84c93bc
Jishnu <jishnupatel1@gmail.com>
2020-12-02 19:02:52 -0500

    created non-functional controllers

--- app/src/main/java/com/example/androidchess26/MainActivity.java
+++ app/src/main/java/com/example/androidchess26/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.androidchess26;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button btnNewGame;
+    Button btnRecordedGames;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNewGame = (Button) findViewById(R.id.btnNewGame);
+        btnRecordedGames = (Button) findViewById(R.id.btnRecordedGames);
+
+        btnNewGame.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRecordedGames.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                Intent intent = new Intent(MainActivity.this, RecordedGamesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
+
 }
