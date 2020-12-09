47d745817721b091cf3f80680ff89d618aab322a
Ujjaval Shah <us89@scarletmail.rutgers.edu>
2020-12-09 18:31:30 -0500

    Added PlaybackActivity.java

---
+++ app/src/main/java/com/example/androidchess26/PlaybackActivity.java
@@ -0,0 +1,17 @@
+package com.example.androidchess26;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PlaybackActivity extends GameActivity implements View.OnClickListener {
+    //message.setText(input);
+    Button btnPrev;
+    Button btnNext;
+}
+
+
