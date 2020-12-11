48ace9237b508a74c3bab3f7381c3191cadb36e0
Jishnu <jishnupatel1@gmail.com>
2020-12-10 19:02:25 -0500

    started PlaybackActivity

--- app/src/main/java/com/example/androidchess26/PlaybackActivity.java
+++ app/src/main/java/com/example/androidchess26/PlaybackActivity.java
@@ -7,16 +7,37 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import model_2.*;
 
-public class PlaybackActivity extends GameActivity implements View.OnClickListener
+public class PlaybackActivity extends AppCompatActivity
 {
-
+    TextView txtturn;
+    TextView txtwinner;
     Button btnPrev;
     Button btnNext;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playback);
 
+        txtturn = (TextView) findViewById(R.id.txtturn);
+        txtwinner = (TextView) findViewById(R.id.txtwinner);
 
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnPrev.setOnClickListener(arg0 -> {
+
+        });
+
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(arg0 -> {
+
+        });
+    }
 
 
 }
